0c37a29f9a8461068003060d3ff91419ba732713
Madson Magalhaes <madson.l.magalhaes@gmail.com>
2026-08-26 15:40:33 -0300

    fix: render GTM script immediately in head via dangerouslySetInnerHTML

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,13 +74,15 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/favicon.png?v=1" />
         {/* Google Tag Manager */}
         {gtmId && (
-          <Script id="google-tag-manager" strategy="afterInteractive">
-            {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${gtmId}');`}
-          </Script>
+})(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
         )}
         {/* End Google Tag Manager */}
         <script
